3b0790b01679a350a125c9ffc8543494424587ce
Sidney Ferracin Jr <sidney.ferracin99@gmail.com>
2023-05-17 10:14:37 -0300

    feat: Adicionado troca de tema e tema escuro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { LightTheme } from "./shared/themes";
-import { ThemeProvider } from "@mui/material";
+import { AppThemeProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
-    </ThemeProvider>
+    <AppThemeProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AppThemeProvider>
   );
 }
